5111eb928a7c8c91ce033f9e42b0373b570821c4
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-03 02:13:38 -0400

    generate JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,52 +1,78 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { genJWT } = require('../helpers/jwt');
 
 const createUser = async (req, res = response) => {
     
     const { email, password } = req.body;
-    let user;
 
     try {
 
         // busca si existe un usuario con el mismo email
-        user = await User.findOne({ email });
+        let user = await User.findOne({ email });
         if( user ) return res.status(400).json({ok: false, msg: "The email is used"})
 
         user = new User( req.body );
-        // Encripting password
+        // Encrypting password
         const salt = bcrypt.genSaltSync();
         user.password = bcrypt.hashSync(password, salt);
         // save in database
         await user.save();
         
+        // Generate JWT
+        const token = await genJWT( user.id, user.name );
+
+        return res.status(201).json({
+            ok: true,
+            msg: "User was created",
+            uid: user.id,
+            name: user.name,
+            token
+        })
+        
     } catch (error) {
+        console.log(error);
         return res.status(500).json({
             ok: false,
             msg: "Please, Talk with the admin"
         })
     }
 
-
-    res.status(201).json({
-        ok: true,
-        msg: "User was created",
-        uid: user.id,
-        name: user.name
-    })
 }
 
 
-const loginUser = (req, res = response) => {
+const loginUser =  async (req, res = response) => {
 
     const { email, password } = req.body;
     
-    res.status(201).json({
-        ok: "Todo ok",
-        msg: "Login",
-        email,
-        password
-    })
+    try {
+    
+        let user = await User.findOne({ email });   
+        if( !user ) return res.status(400).json({ok: false, mgs:"The email or password don't exisit"})
+
+        const validPlassword = bcrypt.compareSync(password, user.password);
+        if( !validPlassword ) return res.status(400).json({ok: false, mgs:"The password is incorrect"})
+
+        // Generate JWT
+        const token = await genJWT( user.id, user.name );
+
+
+        return res.json({
+            ok: true,
+            msg: "User was logged",
+            uid: user.id,
+            name: user.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "Please, talk with the admin"
+        })
+    }
 }
 
 
